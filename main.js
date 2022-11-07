15a47e87cfcf6aafd8ece17d1d99279a9a487315
DuPoisk <julianapoisk@gmail.com>
2022-11-07 00:32:10 -0300

    add assets

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 function createGame(player1, hour, player2) {
   return ` <!-- acentos graves fazem quebra de linha -->
    <li> <!--itens da lista--> 
-    <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+    <img src="./assets/icon-${player1}.svg" alt="Bandeira ${player1}" />
     <strong>${hour}</strong>
-    <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
+    <img src="./assets/icon-${player2}.svg" alt="Bandeira ${player2}" />
   </li>
   `
 }
 
-let delay = -0.4;
+let delay = -0.4
 function createCard(date, day, games) {
-  delay = delay + 0.4;
+  delay = delay + 0.4
   return `
   <div class="card" style="animation=delay: ${delay}s">
       <h2>${date} <span>${day}</span></h2>
@@ -30,17 +30,18 @@ document.querySelector("#app").innerHTML = `
     ${createCard(
       "24/11",
       "quinta",
-      createGame("Brazil", "16:00", "Serbia")
+      createGame("brazil", "16:00", "serbia")
     )} <!-- chamado de interpolação, troca o $ pela função do javascript que eu colocar ali dentro-->
 
     ${createCard(
       "28/11",
       "segunda",
-      createGame("Brazil", "13:00", "switzerland")
+      createGame("brazil", "13:00", "switzerland") +
+        createGame("portugal", "16:00", "uruguay")
     )}
 
   
-    ${createCard("02/12", "sexta", createGame("Brazil", "16:00", "cameroon"))}
+    ${createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))}
     <!-- string sem quebra de linha uso aspas sinmples ' ' -->
   </main>
 `
