c965dbac481fadf0ae822a91c13c759ad4276daf
DuPoisk <julianapoisk@gmail.com>
2022-11-06 22:43:00 -0300

    :tada: initial commit

---
+++ main.js
@@ -0,0 +1,46 @@
+function createGame(player1, hour, player2) {
+  return ` <!-- acentos graves fazem quebra de linha -->
+   <li> <!--itens da lista--> 
+    <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+    <strong>${hour}</strong>
+    <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
+  </li>
+  `
+}
+
+let delay = -0.4;
+function createCard(date, day, games) {
+  delay = delay + 0.4;
+  return `
+  <div class="card" style="animation=delay: ${delay}s">
+      <h2>${date} <span>${day}</span></h2>
+      <ul> <!--lista nao ordenada-->
+       ${games}
+      </ul>
+    </div>
+   <!--  <div class="card2"></div> extra meu-->
+  `
+}
+
+document.querySelector("#app").innerHTML = `
+  <header> <!--cabeçalho-->
+        <img src="./assets/logo.svg" alt="Logo da NLW" />
+  </header>
+  <main id="cards">
+    ${createCard(
+      "24/11",
+      "quinta",
+      createGame("Brazil", "16:00", "Serbia")
+    )} <!-- chamado de interpolação, troca o $ pela função do javascript que eu colocar ali dentro-->
+
+    ${createCard(
+      "28/11",
+      "segunda",
+      createGame("Brazil", "13:00", "switzerland")
+    )}
+
+  
+    ${createCard("02/12", "sexta", createGame("Brazil", "16:00", "cameroon"))}
+    <!-- string sem quebra de linha uso aspas sinmples ' ' -->
+  </main>
+`
